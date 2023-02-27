90f9f42421ced9a535ffbbec993fd787b404cd08
Philip Keller <philipjoelkeller@gmail.com>
2023-02-27 15:34:59 -0800

    Initial Commit

---
+++ GuacChain/GuacChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuacChainApp.swift
+//  GuacChain
+//
+//  Created by Philip Keller on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GuacChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
